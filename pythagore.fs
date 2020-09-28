a541010d778867333f6dcb364b2e33e11b05c9be
francois pussault <goblinrieur@gmail.com>
2020-09-28 20:13:02 +0200

    protetction contre l'usage sur ordi 32bits ou moins

--- pythagore.fs
+++ pythagore.fs
@@ -1,9 +1,13 @@
 #! /usr/local/bin/gforth
 
+cell 8 <> [if] s" 64-bit system required" exception throw [then]
+\ parce que j'accepte des nombres larges  mais bon 
+\ qui a encore du 32 ou du 16bits ... hors collection comme ordi
+
 : >segments  		( -- x y -- sqrt[ x²+y² ] )             
-	cr ." valeur du premier segment : " 
-	pad dup 20 accept >float fdup f*	
-	cr ." valeur du second segment : " 
+	cr ." valeur du premier segment : " 	\ x
+	pad dup 20 accept >float fdup f*
+	cr ." valeur du second segment : " 	\ y 
 	pad dup 20 accept >float fdup f* 
 	f+ 		\ x²+y²
 	fsqrt		\ sqrt 
